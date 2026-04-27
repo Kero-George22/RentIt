3fe7bd3bfd50874290fbd0017ec32471cfd3c02b
Kero-hub <kirolsgorge@gmail.com>
2026-04-27 14:15:18 +0300

    remove software and rentitMain

--- RentIt-main/utils/apiResponse.js
+++
@@ -1,9 +0,0 @@
-function success(res, data = {}, message = 'OK', status = 200) {
-  return res.status(status).json({ success: true, message, data });
-}
-
-function error(res, message = 'Error', status = 500) {
-  return res.status(status).json({ success: false, message });
-}
-
-module.exports = { success, error };
